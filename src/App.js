0a4d42af47533f8537ec455b06d68bef8c00773f
Kiritsu <deebh776@gmail.com>
2024-07-07 13:37:19 +0300

    add load more data button project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-// import StarRating from "./components/star-rating"
+import StarRating from "./components/star-rating"
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 
 function App() {
   return (
     <div className="App">
       {/* <StarRating nOfStars={10}/> */}
 
-      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/>
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/> */}
+
+      <LoadMoreData />
     </div>
   );
 }
